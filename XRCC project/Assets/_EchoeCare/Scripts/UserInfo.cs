7cd0ba9bdd510ce0e5e429afd7b061fb12105868
tangentlab <47337594+tangentlab@users.noreply.github.com>
2025-11-10 15:20:57 -0600

    create user info class

---
+++ XRCC project/Assets/_EchoeCare/Scripts/UserInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace ECHO
+{
+	public class UserInfo
+	{
+		public string userID = "default_user";
+		public string userName = "Default User";
+
+		public UserInfo()
+		{
+			// Constructor logic can be added here if needed
+		}
+	}
+}
