1bb8c8e5bb957dfb02345814066aede492c1e076
tangentlab <47337594+tangentlab@users.noreply.github.com>
2025-11-10 15:21:08 -0600

    create menu handler class

---
+++ XRCC project/Assets/_EchoeCare/Scripts/MenuHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace ECHO
+{
+	public class MenuHandler : MonoBehaviour
+	{
+		//hold list of menus
+
+		internal void InitializeMenu()
+		{
+			// Initialize menu items here
+		}
+	}
+}
