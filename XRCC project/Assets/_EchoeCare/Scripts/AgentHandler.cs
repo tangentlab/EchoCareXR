02d85adf8ae397b4f93ad985d1d74276c89547f5
tangentlab <47337594+tangentlab@users.noreply.github.com>
2025-11-12 13:41:41 -0600

    Update AgentHandler.cs

--- XRCC project/Assets/_EchoeCare/Scripts/AgentHandler.cs
+++ XRCC project/Assets/_EchoeCare/Scripts/AgentHandler.cs
@@ -9,7 +9,7 @@ namespace ECHO
 		// Start is called once before the first execution of Update after the MonoBehaviour is created
 		private void Start()
 		{
-			PlayVoice();
+			//PlayVoice();
 			PositionInfrontOfUser();
 		}
 
