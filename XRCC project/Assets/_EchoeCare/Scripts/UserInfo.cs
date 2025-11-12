7044af883126f71d2183d64c7ad6edb844532773
tangentlab <guidewire@gmail.com>
2025-11-12 00:19:33 -0600

    Update UserInfo.cs

--- XRCC project/Assets/_EchoeCare/Scripts/UserInfo.cs
+++ XRCC project/Assets/_EchoeCare/Scripts/UserInfo.cs
@@ -5,8 +5,11 @@
 		public string userID = "default_user";
 		public string userName = "Default User";
 
-		public UserInfo()
+		public UserInfo(string name = "Default User", string id = "default_user")
 		{
+			userName = name;
+			userID = id;
+
 			// Constructor logic can be added here if needed
 		}
 	}
