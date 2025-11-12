a8fc5c574691c0d09d3db41929fe53e998cacd77
tangentlab <guidewire@gmail.com>
2025-11-11 23:26:09 -0600

    Update MenuHandler.cs

--- XRCC project/Assets/_EchoeCare/Scripts/MenuHandler.cs
+++ XRCC project/Assets/_EchoeCare/Scripts/MenuHandler.cs
@@ -5,11 +5,32 @@ namespace ECHO
 {
 	public class MenuHandler : MonoBehaviour
 	{
+		public GameObject welcomeMenu;
+		public GameObject dateMenu;
+
 		//hold list of menus
 
 		internal void InitializeMenu()
 		{
 			// Initialize menu items here
+			if (welcomeMenu != null)
+			{
+				welcomeMenu.SetActive(true);
+			}
+			else
+			{
+				Debug.LogWarning("WelcomeMenu is not assigned in MenuHandler.");
+			}
+
+			if (dateMenu != null)
+			{
+				dateMenu.SetActive(false);
+			}
+			else
+			{
+				Debug.LogWarning("MainMenu is not assigned in MenuHandler.");
+			}
+
 		}
 	}
 }
