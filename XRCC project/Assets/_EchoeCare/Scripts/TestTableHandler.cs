066b525542300d272b5d77cefc192a296643e545
tangentlab <guidewire@gmail.com>
2025-11-11 23:25:45 -0600

    create class to handle table model test

---
+++ XRCC project/Assets/_EchoeCare/Scripts/TestTableHandler.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+namespace ECHO
+{
+
+    public class TestTableHandler : MonoBehaviour
+    {
+        public GameObject applePrefab;
+        public GameObject glassesPrefab;
+        public GameObject keyPrefab;
+        public GameObject potPrefab;
+        public GameObject cupPrefab;
+
+
+
+        public Transform tableTransform;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+
+        }
+
+
+    }
+}
