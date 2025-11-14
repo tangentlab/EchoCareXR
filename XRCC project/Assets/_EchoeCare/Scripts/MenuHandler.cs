de1d080c65588d30e98b04775a4e04f07ae87315
tangentlab <47337594+tangentlab@users.noreply.github.com>
2025-11-13 23:56:51 -0600

    Update MenuHandler.cs
    attempt menu positioning function

--- XRCC project/Assets/_EchoeCare/Scripts/MenuHandler.cs
+++ XRCC project/Assets/_EchoeCare/Scripts/MenuHandler.cs
@@ -17,12 +17,28 @@ namespace ECHO
 		{
 			// Initialize menu items here
 			if (welcomeMenu) welcomeMenu.SetActive(true);
+			//PostitionMenuInFront();
 
 			if (dateMenu) dateMenu.SetActive(false);
 			if (doNextMenu) doNextMenu.SetActive(false);
 
 			// hide welcome menu after 5 seconds and show date menu
 			Invoke(nameof(ShowDateMenu), welcomeMenuDuration);
+		}
+
+		private void PostitionMenuInFront()
+		{
+			//position menu in front of user
+
+			const float dist = 1.0f;
+			Transform userTransform = Camera.main.transform;
+			Vector3 forward = new Vector3(userTransform.forward.x, 0, userTransform.forward.z).normalized;
+			Vector3 targetPosition = userTransform.position + forward * dist; // 2 meters in front
+
+			targetPosition.y = userTransform.position.y; //plane the companion on the ground level
+
+			transform.position = targetPosition;
+			transform.LookAt(new Vector3(userTransform.position.x, transform.position.y, userTransform.position.z));
 		}
 
 		private void ShowDateMenu()
