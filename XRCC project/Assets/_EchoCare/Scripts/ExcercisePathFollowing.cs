c1b011305bece893e84787cd32e3c974919d17bf
tangentlab <guidewire@gmail.com>
2025-11-15 02:48:45 -0600

    Update ExcercisePathFollowing.cs

--- XRCC project/Assets/_EchoCare/Scripts/ExcercisePathFollowing.cs
+++ XRCC project/Assets/_EchoCare/Scripts/ExcercisePathFollowing.cs
@@ -13,14 +13,9 @@ namespace ECHO
 			echoManager.testTableHandler.ShowPathFollowing();
 		}
 
-		// Start is called once before the first execution of Update after the MonoBehaviour is created
-		private void Start()
+		private void Awake()
 		{
-		}
-
-		// Update is called once per frame
-		private void Update()
-		{
+			echoManager = EchoManager.Instance;
 		}
 	}
 }
