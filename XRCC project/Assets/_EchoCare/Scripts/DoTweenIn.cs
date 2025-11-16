86601214372f246f495a22485c748610d82ebb9e
tangentlab <47337594+tangentlab@users.noreply.github.com>
2025-11-15 21:25:56 -0600

    Update DoTweenIn.cs

--- XRCC project/Assets/_EchoCare/Scripts/DoTweenIn.cs
+++ XRCC project/Assets/_EchoCare/Scripts/DoTweenIn.cs
@@ -13,7 +13,19 @@ namespace ECHO
 			if (hasTweenedIn) return;
 
 			transform.DOScale(Vector3.one, 0.5f).From(Vector3.zero).SetEase(Ease.OutBack);
+			//hasTweenedIn = true;
+		}
+
+		private void OnTriggerEnter(Collider other)
+		{
+			//print("-- DoTweenIn Triggered by: " + other.name);
 			hasTweenedIn = true;
+			transform.localScale = Vector3.one;
+		}
+
+		private void OnTriggerExit(Collider other)
+		{
+			transform.localScale = Vector3.one;
 		}
 	}
 }
